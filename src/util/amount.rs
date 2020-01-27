2cac427d7d36ba828f4f70c30779de874500053c
Marty Jones <murtin.jones@gmail.com>
2020-01-26 21:22:28 -0500

    add support for multiplication, remainder, modulus

--- src/util/amount.rs
+++ src/util/amount.rs
@@ -4,6 +4,8 @@ use std::fmt::{self, Write};
 use std::ops;
 use std::str::FromStr;
 
+/// TODO there should be a way to implement a build script for this and for SignedAmount
+
 /// Can be used to represent Bitcoin amounts. Supports
 /// arithmatic operations.
 #[derive(Copy, Clone, Hash)]
@@ -30,6 +32,25 @@ impl Amount {
     /// @see https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedSub.html
     pub fn checked_sub(self, rhs: Amount) -> Option<Amount> {
         self.0.checked_sub(rhs.0).map(Amount)
+    }
+
+    /// Performs a checked multiplication, returning None if an overflow occurs.
+    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedMul.html
+    pub fn checked_mul(self, rhs: u64) -> Option<Amount> {
+        self.0.checked_mul(rhs).map(Amount)
+    }
+
+    /// Performs a checked division, returning None if an overflow occurs.
+    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedDiv.html
+    /// NOTE: The remainder will be lost if no exact division takes place.
+    pub fn checked_div(self, rhs: u64) -> Option<Amount> {
+        self.0.checked_div(rhs).map(Amount)
+    }
+
+    /// Performs a checked remainder, returning None if an overflow occurs.
+    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedDiv.html
+    pub fn checked_rem(self, rhs: u64) -> Option<Amount> {
+        self.0.checked_rem(rhs).map(Amount)
     }
 }
 
@@ -65,6 +86,31 @@ impl fmt::Debug for Amount {
     }
 }
 
+/// Allows us to use `*` to multiply Amounts
+impl ops::Mul<u64> for Amount {
+    type Output = Self;
+
+    fn mul(self, rhs: u64) -> Self {
+        self.checked_mul(rhs).expect("Uh oh! Multiplcation error")
+    }
+}
+
+impl ops::Div<u64> for Amount {
+    type Output = Self;
+
+    fn div(self, rhs: u64) -> Self {
+        self.checked_div(rhs).expect("Uh oh! Division error")
+    }
+}
+
+impl ops::Rem<u64> for Amount {
+    type Output = Self;
+
+    fn rem(self, modulus: u64) -> Self {
+        self.checked_rem(modulus).expect("Uh oh! Remainder error")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -86,5 +132,8 @@ mod tests {
 
         assert_eq!(sat(15) + sat(15), sat(30));
         assert_eq!(sat(15) - sat(15), sat(0));
+        assert_eq!(sat(15) * 10, sat(150));
+        assert_eq!(sat(15) / 5, sat(3));
+        assert_eq!(sat(14) % 5, sat(4));
     }
 }
