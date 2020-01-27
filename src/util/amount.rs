f72b36b34ff2f7a768450f3f7630f78769283358
Marty Jones <murtin.jones@gmail.com>
2020-01-26 20:49:07 -0500

    addition/subtraction

--- src/util/amount.rs
+++ src/util/amount.rs
@@ -15,30 +15,53 @@ impl Amount {
         Amount(satoshis)
     }
 
+    /// Get the number of satoshis
+    pub fn as_sat(self) -> u64 {
+        self.0
+    }
+
     /// Performs a 'checked' addition. Returns `None` if an overflow occurs.
-    pub fn checked_add(self, rhs: Amount) -> Opton<Amount> {
+    /// @see https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedAdd.html
+    pub fn checked_add(self, rhs: Amount) -> Option<Amount> {
         self.0.checked_add(rhs.0).map(Amount)
+    }
+
+    /// Performs a 'checked' substrction. Returns `None` if an overflow occurs.
+    /// @see https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedSub.html
+    pub fn checked_sub(self, rhs: Amount) -> Option<Amount> {
+        self.0.checked_sub(rhs.0).map(Amount)
     }
 }
 
-/// Implements the `+` operator for Amount instances
+/// Implements the `+` operator using a checked addition for Amount instances.
 impl ops::Add for Amount {
-    /// TODO Can this just be `Self`?
-    type Output = Amount;
+    /// bitcoin-rs uses `type Output = Amount;` here for some reason:
+    type Output = Self;
 
     fn add(self, rhs: Amount) -> Self::Output {
         self.checked_add(rhs).expect("Whoops! Addition error")
     }
 }
 
-/// Can be used to represent Bitcoin amounts. Supports
-/// arithmatic operations.
-pub struct SignedAmount(i64);
+impl ops::Sub for Amount {
+    type Output = Self;
 
-impl SignedAmount {
-    /// Creates an Amount object from a given number of satoshis
-    pub fn from_sat(satoshis: i64) -> SignedAmount {
-        SignedAmount(satoshis)
+    fn sub(self, rhs: Amount) -> Self::Output {
+        self.checked_sub(rhs).expect("Whoops! Subtraction error")
+    }
+}
+
+/// Allows us to compare Satoshi amounts using `==`
+impl PartialEq for Amount {
+    fn eq(&self, other: &Amount) -> bool {
+        PartialEq::eq(&self.0, &other.0)
+    }
+}
+
+/// Allows us to display amounts for Satoshis and compare them in tests
+impl fmt::Debug for Amount {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Amount({} satoshi)", self.as_sat())
     }
 }
 
@@ -54,8 +77,8 @@ mod tests {
     #[test]
     fn test_add_subtract_multiply_divide() {
         let sat = Amount::from_sat;
-        let ssat = SignedAmount::from_sat;
 
         assert_eq!(sat(15) + sat(15), sat(30));
+        assert_eq!(sat(15) - sat(15), sat(0));
     }
 }
