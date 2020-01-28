36205efb0bc8c29e8af28d6a5ff4240d24e1083e
Marty Jones <murtin.jones@gmail.com>
2020-01-27 19:48:05 -0500

    -assign traits

--- src/util/amount.rs
+++ src/util/amount.rs
@@ -52,6 +52,11 @@ impl Amount {
     pub fn checked_rem(self, rhs: u64) -> Option<Amount> {
         self.0.checked_rem(rhs).map(Amount)
     }
+
+    /// The max allowed value of a SignedAmount
+    pub fn max_value() -> Amount {
+        Amount(u64::max_value())
+    }
 }
 
 /// Implements the `+` operator using a checked addition for Amount instances.
@@ -64,12 +69,26 @@ impl ops::Add for Amount {
     }
 }
 
+/// Allows `+=`
+impl ops::AddAssign for Amount {
+    fn add_assign(&mut self, other: Amount) {
+        *self = *self + other
+    }
+}
+
 /// Allows us to subtract one Amount from another using `-`
 impl ops::Sub for Amount {
     type Output = Self;
 
     fn sub(self, rhs: Amount) -> Self::Output {
         self.checked_sub(rhs).expect("Whoops! Subtraction error")
+    }
+}
+
+/// Allows `-=`
+impl ops::SubAssign for Amount {
+    fn sub_assign(&mut self, other: Amount) {
+        *self = *self - other
     }
 }
 
@@ -96,6 +115,13 @@ impl ops::Mul<u64> for Amount {
     }
 }
 
+/// Allows `*=`
+impl ops::MulAssign<u64> for Amount {
+    fn mul_assign(&mut self, rhs: u64) {
+        *self = *self * rhs
+    }
+}
+
 /// Allows us to use `/` to divide Amounts
 impl ops::Div<u64> for Amount {
     type Output = Self;
@@ -105,12 +131,26 @@ impl ops::Div<u64> for Amount {
     }
 }
 
+/// Allows `/=`
+impl ops::DivAssign<u64> for Amount {
+    fn div_assign(&mut self, rhs: u64) {
+        *self = *self / rhs
+    }
+}
+
 /// Allows us to use `%` to find remainders from dividing Amounts
 impl ops::Rem<u64> for Amount {
     type Output = Self;
 
     fn rem(self, modulus: u64) -> Self {
         self.checked_rem(modulus).expect("Uh oh! Remainder error")
+    }
+}
+
+/// Allows `%=`
+impl ops::RemAssign<u64> for Amount {
+    fn rem_assign(&mut self, other: u64) {
+        *self = *self % other
     }
 }
 
@@ -158,6 +198,11 @@ impl SignedAmount {
     pub fn checked_rem(self, rhs: i64) -> Option<SignedAmount> {
         self.0.checked_rem(rhs).map(SignedAmount)
     }
+
+    /// The max allowed value of a SignedAmount
+    pub fn max_value() -> SignedAmount {
+        SignedAmount(i64::max_value())
+    }
 }
 
 /// Implements the `+` operator using a checked addition for SignedAmount instances.
@@ -170,12 +215,26 @@ impl ops::Add for SignedAmount {
     }
 }
 
+/// Allows `+=`
+impl ops::AddAssign for SignedAmount {
+    fn add_assign(&mut self, other: SignedAmount) {
+        *self = *self + other
+    }
+}
+
 /// Allows us to subtract one SignedAmount from another using `-`
 impl ops::Sub for SignedAmount {
     type Output = Self;
 
     fn sub(self, rhs: SignedAmount) -> Self::Output {
         self.checked_sub(rhs).expect("Whoops! Subtraction error")
+    }
+}
+
+/// Allows `-=`
+impl ops::SubAssign for SignedAmount {
+    fn sub_assign(&mut self, other: SignedAmount) {
+        *self = *self - other
     }
 }
 
@@ -202,6 +261,13 @@ impl ops::Mul<i64> for SignedAmount {
     }
 }
 
+/// Allows `*=`
+impl ops::MulAssign<i64> for SignedAmount {
+    fn mul_assign(&mut self, rhs: i64) {
+        *self = *self * rhs
+    }
+}
+
 /// Allows us to use `/` to divide SignedAmounts
 impl ops::Div<i64> for SignedAmount {
     type Output = Self;
@@ -211,12 +277,26 @@ impl ops::Div<i64> for SignedAmount {
     }
 }
 
+/// Allows `/=`
+impl ops::DivAssign<i64> for SignedAmount {
+    fn div_assign(&mut self, rhs: i64) {
+        *self = *self / rhs
+    }
+}
+
 /// Allows us to use `%` to find remainders from dividing SignedAmounts
 impl ops::Rem<i64> for SignedAmount {
     type Output = Self;
 
     fn rem(self, modulus: i64) -> Self {
         self.checked_rem(modulus).expect("Uh oh! Remainder error")
+    }
+}
+
+/// Allows `%=`
+impl ops::RemAssign<i64> for SignedAmount {
+    fn rem_assign(&mut self, other: i64) {
+        *self = *self % other
     }
 }
 
@@ -240,11 +320,44 @@ mod tests {
         let sat = Amount::from_sat;
         let ssat = SignedAmount::from_sat;
 
+        // Basic arithmetic
         assert_eq!(sat(15) + sat(15), sat(30));
         assert_eq!(sat(15) - sat(15), sat(0));
         assert_eq!(sat(15) * 10, sat(150));
         assert_eq!(sat(15) / 5, sat(3));
         assert_eq!(sat(14) % 5, sat(4));
         assert_eq!(ssat(15) - ssat(20), ssat(-5));
+        assert_eq!(ssat(-14) * 3, ssat(-42));
+        assert_eq!(ssat(-14) % 3, ssat(-2));
+
+        // test -Assign traits
+        let mut bs = ssat(-5);
+        bs += ssat(16);
+        assert_eq!(ssat(11), bs);
+        bs -= ssat(3);
+        assert_eq!(ssat(8), bs);
+        bs /= 2;
+        assert_eq!(ssat(4), bs);
+        bs %= 3;
+        assert_eq!(ssat(1), bs);
+        bs *= 14;
+        assert_eq!(ssat(14), bs);
+        let mut b = sat(5);
+        b += sat(16);
+        assert_eq!(sat(21), b);
+        b -= sat(3);
+        assert_eq!(sat(18), b);
+        b /= 2;
+        assert_eq!(sat(9), b);
+        b %= 5;
+        assert_eq!(sat(4), b);
+        b *= 2;
+        assert_eq!(sat(8), b);
+
+        // panic when overflow occurs
+        let result = panic::catch_unwind(|| Amount::max_value() + Amount::from_sat(1));
+        assert!(result.is_err());
+        let result = panic::catch_unwind(|| Amount::from_sat(8446744073709551615) * 3);
+        assert!(result.is_err());
     }
 }
