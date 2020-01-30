ef100f349054c534830b870ab9d1d6b550dfe7c4
Marty Jones <murtin.jones@gmail.com>
2020-01-29 21:03:29 -0500

    generic trait implementations

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -46,10 +46,44 @@ fn impl_formulate(ast: &syn::DeriveInput) -> TokenStream {
             pub fn from_sat(satoshis: #num_type) -> #struct_name {
                 #struct_name(satoshis)
             }
+            /// Get the number of satoshis
+            pub fn as_sat(self) -> #num_type {
+                self.0
+            }
             /// Performs a 'checked' addition. Returns `None` if an overflow occurs.
             /// @see https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedAdd.html
             pub fn checked_add(self, rhs: #struct_name) -> Option<#struct_name> {
                 self.0.checked_add(rhs.0).map(#struct_name)
+            }
+            /// Performs a 'checked' subtraction. Returns `None` if an overflow occurs.
+            /// @see https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedSub.html
+            pub fn checked_sub(self, rhs: #struct_name) -> Option<#struct_name> {
+                self.0.checked_sub(rhs.0).map(#struct_name)
+            }
+            /// Performs a checked multiplication, returning None if an overflow occurs.
+            /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedMul.html
+            pub fn checked_mul(self, rhs: #num_type) -> Option<#struct_name> {
+                self.0.checked_mul(rhs).map(#struct_name)
+            }
+            /// Performs a checked division, returning None if an overflow occurs.
+            /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedDiv.html
+            /// NOTE: The remainder will be lost if no exact division takes place.
+            pub fn checked_div(self, rhs: #num_type) -> Option<#struct_name> {
+                self.0.checked_div(rhs).map(#struct_name)
+            }
+            /// Performs a checked remainder, returning None if an overflow occurs.
+            /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedDiv.html
+            pub fn checked_rem(self, rhs: #num_type) -> Option<#struct_name> {
+                self.0.checked_rem(rhs).map(#struct_name)
+            }
+            /// The max allowed value of a Amount
+            pub fn max_value() -> #struct_name {
+                #struct_name(#num_type::max_value())
+            }
+
+            /// The min allowed value of a Amount
+            pub fn min_value() -> #struct_name {
+                #struct_name(#num_type::min_value())
             }
         }
 
@@ -59,6 +93,22 @@ fn impl_formulate(ast: &syn::DeriveInput) -> TokenStream {
 
             fn add(self, rhs: #struct_name) -> Self::Output {
                 self.checked_add(rhs).expect("Whoops! Addition error")
+            }
+        }
+
+        /// Allows us to use `*` to multiply Amounts
+        impl ops::Mul<#num_type> for #struct_name {
+            type Output = Self;
+
+            fn mul(self, rhs: #num_type) -> Self {
+                self.checked_mul(rhs).expect("Uh oh! Multiplcation error")
+            }
+        }
+
+        /// Allows `*=`
+        impl ops::MulAssign<#num_type> for #struct_name {
+            fn mul_assign(&mut self, rhs: #num_type) {
+                *self = *self * rhs
             }
         }
 
@@ -82,6 +132,38 @@ fn impl_formulate(ast: &syn::DeriveInput) -> TokenStream {
         impl ops::SubAssign for #struct_name {
             fn sub_assign(&mut self, other: #struct_name) {
                 *self = *self - other
+            }
+        }
+
+        /// Allows us to use `/` to divide Amounts
+        impl ops::Div<#num_type> for #struct_name {
+            type Output = Self;
+
+            fn div(self, rhs: #num_type) -> Self {
+                self.checked_div(rhs).expect("Uh oh! Division error")
+            }
+        }
+
+        /// Allows `/=`
+        impl ops::DivAssign<#num_type> for #struct_name {
+            fn div_assign(&mut self, rhs: #num_type) {
+                *self = *self / rhs
+            }
+        }
+
+        /// Allows us to use `%` to find remainders from dividing Amounts
+        impl ops::Rem<#num_type> for #struct_name {
+            type Output = Self;
+
+            fn rem(self, modulus: #num_type) -> Self {
+                self.checked_rem(modulus).expect("Uh oh! Remainder error")
+            }
+        }
+
+        /// Allows `%=`
+        impl ops::RemAssign<#num_type> for #struct_name {
+            fn rem_assign(&mut self, other: #num_type) {
+                *self = *self % other
             }
         }
     };
--- src/util/amount.rs
+++ src/util/amount.rs
@@ -11,48 +11,6 @@ use std::str::FromStr;
 #[derive(Copy, Clone, Hash, PartialEq, SharedAmountTraits)]
 pub struct Amount(u64);
 
-impl Amount {
-    /// Get the number of satoshis
-    pub fn as_sat(self) -> u64 {
-        self.0
-    }
-
-    /// Performs a 'checked' substrction. Returns `None` if an overflow occurs.
-    /// @see https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedSub.html
-    pub fn checked_sub(self, rhs: Amount) -> Option<Amount> {
-        self.0.checked_sub(rhs.0).map(Amount)
-    }
-
-    /// Performs a checked multiplication, returning None if an overflow occurs.
-    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedMul.html
-    pub fn checked_mul(self, rhs: u64) -> Option<Amount> {
-        self.0.checked_mul(rhs).map(Amount)
-    }
-
-    /// Performs a checked division, returning None if an overflow occurs.
-    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedDiv.html
-    /// NOTE: The remainder will be lost if no exact division takes place.
-    pub fn checked_div(self, rhs: u64) -> Option<Amount> {
-        self.0.checked_div(rhs).map(Amount)
-    }
-
-    /// Performs a checked remainder, returning None if an overflow occurs.
-    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedDiv.html
-    pub fn checked_rem(self, rhs: u64) -> Option<Amount> {
-        self.0.checked_rem(rhs).map(Amount)
-    }
-
-    /// The max allowed value of a Amount
-    pub fn max_value() -> Amount {
-        Amount(u64::max_value())
-    }
-
-    /// The min allowed value of a Amount
-    pub fn min_value() -> Amount {
-        Amount(u64::min_value())
-    }
-}
-
 /// Allows us to display amounts for Satoshis and compare them in tests
 impl fmt::Debug for Amount {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -60,151 +18,13 @@ impl fmt::Debug for Amount {
     }
 }
 
-/// Allows us to use `*` to multiply Amounts
-impl ops::Mul<u64> for Amount {
-    type Output = Self;
-
-    fn mul(self, rhs: u64) -> Self {
-        self.checked_mul(rhs).expect("Uh oh! Multiplcation error")
-    }
-}
-
-/// Allows `*=`
-impl ops::MulAssign<u64> for Amount {
-    fn mul_assign(&mut self, rhs: u64) {
-        *self = *self * rhs
-    }
-}
-
-/// Allows us to use `/` to divide Amounts
-impl ops::Div<u64> for Amount {
-    type Output = Self;
-
-    fn div(self, rhs: u64) -> Self {
-        self.checked_div(rhs).expect("Uh oh! Division error")
-    }
-}
-
-/// Allows `/=`
-impl ops::DivAssign<u64> for Amount {
-    fn div_assign(&mut self, rhs: u64) {
-        *self = *self / rhs
-    }
-}
-
-/// Allows us to use `%` to find remainders from dividing Amounts
-impl ops::Rem<u64> for Amount {
-    type Output = Self;
-
-    fn rem(self, modulus: u64) -> Self {
-        self.checked_rem(modulus).expect("Uh oh! Remainder error")
-    }
-}
-
-/// Allows `%=`
-impl ops::RemAssign<u64> for Amount {
-    fn rem_assign(&mut self, other: u64) {
-        *self = *self % other
-    }
-}
-
 #[derive(Copy, Clone, Hash, PartialEq, SharedAmountTraits)]
 pub struct SignedAmount(i64);
-
-impl SignedAmount {
-    /// Get the number of satoshis
-    pub fn as_sat(self) -> i64 {
-        self.0
-    }
-
-    /// Performs a 'checked' substrction. Returns `None` if an overflow occurs.
-    /// @see https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedSub.html
-    pub fn checked_sub(self, rhs: SignedAmount) -> Option<SignedAmount> {
-        self.0.checked_sub(rhs.0).map(SignedAmount)
-    }
-
-    /// Performs a checked multiplication, returning None if an overflow occurs.
-    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedMul.html
-    pub fn checked_mul(self, rhs: i64) -> Option<SignedAmount> {
-        self.0.checked_mul(rhs).map(SignedAmount)
-    }
-
-    /// Performs a checked division, returning None if an overflow occurs.
-    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedDiv.html
-    /// NOTE: The remainder will be lost if no exact division takes place.
-    pub fn checked_div(self, rhs: i64) -> Option<SignedAmount> {
-        self.0.checked_div(rhs).map(SignedAmount)
-    }
-
-    /// Performs a checked remainder, returning None if an overflow occurs.
-    /// https://rust-num.github.io/num/num_traits/ops/checked/trait.CheckedDiv.html
-    pub fn checked_rem(self, rhs: i64) -> Option<SignedAmount> {
-        self.0.checked_rem(rhs).map(SignedAmount)
-    }
-
-    /// The max allowed value of a SignedAmount
-    pub fn max_value() -> SignedAmount {
-        SignedAmount(i64::max_value())
-    }
-
-    /// The min allowed value of a SignedAmount
-    pub fn min_value() -> SignedAmount {
-        SignedAmount(i64::min_value())
-    }
-}
 
 /// Allows us to display amounts for Satoshis and compare them in tests
 impl fmt::Debug for SignedAmount {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "SignedAmount({} satoshi)", self.as_sat())
-    }
-}
-
-/// Allows us to use `*` to multiply SignedAmounts
-impl ops::Mul<i64> for SignedAmount {
-    type Output = Self;
-
-    fn mul(self, rhs: i64) -> Self {
-        self.checked_mul(rhs).expect("Uh oh! Multiplcation error")
-    }
-}
-
-/// Allows `*=`
-impl ops::MulAssign<i64> for SignedAmount {
-    fn mul_assign(&mut self, rhs: i64) {
-        *self = *self * rhs
-    }
-}
-
-/// Allows us to use `/` to divide SignedAmounts
-impl ops::Div<i64> for SignedAmount {
-    type Output = Self;
-
-    fn div(self, rhs: i64) -> Self {
-        self.checked_div(rhs).expect("Uh oh! Division error")
-    }
-}
-
-/// Allows `/=`
-impl ops::DivAssign<i64> for SignedAmount {
-    fn div_assign(&mut self, rhs: i64) {
-        *self = *self / rhs
-    }
-}
-
-/// Allows us to use `%` to find remainders from dividing SignedAmounts
-impl ops::Rem<i64> for SignedAmount {
-    type Output = Self;
-
-    fn rem(self, modulus: i64) -> Self {
-        self.checked_rem(modulus).expect("Uh oh! Remainder error")
-    }
-}
-
-/// Allows `%=`
-impl ops::RemAssign<i64> for SignedAmount {
-    fn rem_assign(&mut self, other: i64) {
-        *self = *self % other
     }
 }
 
